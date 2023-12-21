75c1a62e346a9e71d23b65a21785a37f9aa2d3c0
daria_kafler <me@dariakafler.com>
2023-12-21 16:10:40 +0000

    add initial validation to donation request

--- src/donation.ts
+++ src/donation.ts
@@ -5,8 +5,31 @@ import { Donation, DonationPostBody } from './types';
 var donations: Donation[];
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<any> => {
+  const body: DonationPostBody = JSON.parse(event.body);
+
+  if (!body.full_name) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'no name' })
+    }
+  }
+
+  if (!body.email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'no email' })
+    }
+  }
+
+  if (!body.sum) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'no sum' })
+    }
+  }
+
   return {
     statusCode: 200, 
-    body: JSON.stringify({ 'res': donations })
+    body: JSON.stringify({ 'req': JSON.stringify(body), 'res': JSON.stringify(donations) })
   }
 };
